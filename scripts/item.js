55c36af2c50d741250084f57a3f1d76038b10a9e
jorgeespino  Student <selectjorge@gmail.com>
2020-01-13 15:06:53 -0600

    continued assignment exercises

--- scripts/item.js
+++ scripts/item.js
@@ -1,1 +1,18 @@
-export default {};
+function validateName (name) {
+  if (name.length === 0) {
+      throw TypeError ('name must not be blank');
+    }
+
+
+}
+function create (name) {
+return {
+    id: cuid(),
+    name,
+    checked: false
+}    
+}
+export default 
+{validateName,create};
+ 
+ 
--- scripts/store.js
+++ scripts/store.js
@@ -2,6 +2,23 @@ import item from './item.js';
 
 const items = [];
 const hideCheckedItems = false;
+function findbyid(id) {
+  return items.find(item => item.id === id); 
+}
+
+function addItem(name) {
+try {
+  item.validateName(name);
+  this.items.push(item.create(name));
+}
+catch (error) {
+  console.log(`cannot add item: ${error.message}`);
+}
+
+}
+
+function findAndToggleChecked (id)
+this.findbyId.fetch == false)  
 
 export default {
   items,
